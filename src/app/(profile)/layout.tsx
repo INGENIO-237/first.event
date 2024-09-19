f47bfd632db4aa027a7ae82c4fa08e5c7319e7dd
INGENIO <ingeniokhalif@gmail.com>
2024-09-19 11:57:47 +0100

    completed add payment method and formatted code

--- src/app/(profile)/layout.tsx
+++ src/app/(profile)/layout.tsx
@@ -4,20 +4,18 @@ import SideBar from "./_components/SideBar";
 import "react-country-state-city/dist/react-country-state-city.css";
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <NavBar />
-            <MobNavBar />
-            <div className="flex flex-row">
-                <SideBar />
-                <div className="px-4 py-2 md:max-w-[80%] w-full">
-                {children}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <MobNavBar />
+      <div className="flex flex-row">
+        <SideBar />
+        <div className="px-4 py-2 md:max-w-[80%] w-full">{children}</div>
+      </div>
+    </div>
+  );
 }
