cdb7e07c7c1320d1ea6f7633f9b348675e2568e0
Jiordi Viera <jiordikengne@gmail.com>
2024-10-10 16:57:53 +0100

    register page integration

--- src/utils/types/auth.ts
+++ src/utils/types/auth.ts
@@ -1,15 +1,26 @@
-import { forgotPasswordSchema, loginSchema, otpConfirmSchema } from "@/schema/AuthValidation";
+import {forgotPasswordSchema, loginSchema, otpConfirmSchema, registerSchema} from "@/schema/AuthValidation";
 import * as z from "zod";
 
 
 export type LoginData = z.infer<typeof loginSchema>;
+export type RegisterData = z.infer<typeof registerSchema>
 
 export type LoginResponse = {
     accessToken: string;
     refreshToken: string;
     otpGenerated: boolean;
 };
-
+export interface RegisterResponse {
+    _id: string;
+    email: string;
+    firstname: string;
+    lastname: string;
+    isVerified: boolean;
+    hasBeenDeleted: boolean;
+    interests: unknown[];
+    createdAt: Date;
+    updatedAt: Date;
+}
 export type forgotPassword = z.infer<typeof forgotPasswordSchema>;
 
 export type OTPData = z.infer<typeof otpConfirmSchema>;
